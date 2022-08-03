b1e8181cab4750337af95859b0aad46ba911c06f
leonardo arena <61668570+leofa741@users.noreply.github.com>
2022-08-03 18:21:16 -0300

    -

--- routes/article.js
+++ routes/article.js
@@ -5,8 +5,8 @@ const ArticleController = require('../controllers/article');
 
 var router = express.Router();
 
-var multipart = require('connect-multiparty');
-var md_upload = multipart({ uploadDir: './upload/articles'});
+const multipart = require('connect-multiparty');
+const md_upload = multipart({ uploadDir: './upload/articles'});
 
 // Rutas de prueba
 router.post('/datos-prueba', ArticleController.datosPrueba);
